c4aa11b638c31e0baf092b9e86c0a260e2af6d06
Guy-L <g.guy.lab@gmail.com>
2024-05-30 15:02:20 -0400

    Add formatting to setup & version checker scripts, improve error handling during setup

--- parakit.py
+++ parakit.py
@@ -1,6 +1,10 @@
 def input_exit():
     print()
-    input("Press Enter to close the program. ")
+    try:
+        input("\33[5mPress \33[1mEnter\033[22m to close the program.\033[25m")
+    except KeyboardInterrupt:
+        print()
+    print("\033[F\033[K\033[F", end='')
     exit()
 
 #Extreme sanity check
@@ -10,64 +14,104 @@ try:
     import os
     import platform
     import subprocess
-except ImportError as e:
-    print(f"Setup error: Error while importing standard libraries: {e}\nIf you see this message, please contact the developer!")
+    import traceback
+    import shutil
+    from utils import *
+except Exception as e:
+    print(f"\33[31mSetup error:\33[0m Error while importing standard libraries: {e}")
+    print("If you see this message, please contact the developer!")
     input_exit()
 
-_venv_path = 'venv'
-_reqs_path = 'requirements.txt'
-_worker_script_path = 'state_reader.py'
-_vcheck_script_path = 'version_check.py'
+def valid_venv(bin_folder, pr=False):
+    missing = []
 
-#Create venv if not already created
-if not os.path.isfile(os.path.join(_venv_path, 'pyvenv.cfg')):
-    if os.path.exists(_venv_path):
-        print(f"Setup error: The venv folder at '{_venv_path}' appears to be corrupted (missing config file).")
-        print("Fix the issue or delete the folder and run this script again.")
+    if not os.path.exists(os.path.join(_venv_path, 'pyvenv.cfg')):
+        missing.append('pyvenv.cfg')
+    if not os.path.exists(os.path.join(_venv_path, bin_folder, 'pip.exe')):
+        missing.append('pip')
+    if not os.path.exists(os.path.join(_venv_path, bin_folder, 'python.exe')):
+        missing.append('python')
+
+    if pr and missing:
+        print(darker(color(f"Invalid venv for bin folder '{bin_folder}': ", 'red') + f"Missing: {', '.join(missing)}"))
+    return not bool(missing)
+
+try:
+    _venv_path = 'venv'
+    _reqs_path = 'requirements.txt'
+    _worker_script_path = 'state_reader.py'
+    _vcheck_script_path = 'version_check.py'
+
+    #Create venv if not already created
+    if not os.path.exists(_venv_path):
+        print(color(bright("\nIf this is your first time using ParaKit, welcome!"), 'green'))
+        print(color("Please give us a moment to finish setting things up.", 'green'))
+        print(color(darker("It should take about a minute.\n"), 'green'))
+
+        print(bright("First Time Setup:"), f"Creating virtual environment at '{bright(_venv_path)}'...")
+        venv.create(_venv_path, with_pip=True)
+        print(bright("First Time Setup:"), f"Virtual environment created at '{bright(_venv_path)}'.")
+
+        print(bright("First Time Setup:"), f"Installing dependencies...")
+        print(bright("Tip:"), "If it seems to do no progress at all for a while, try pressing Enter.", italics(darker("Python's weird.\n")))
+    else:
+        print(italics(darker("Checking dependencies...")), end='\r')
+
+    if valid_venv('Scripts', True):
+        _pip_exe = os.path.join(_venv_path, 'Scripts', 'pip')
+        _python_exe = os.path.join(_venv_path, 'Scripts', 'python')
+
+    elif valid_venv('bin', True):
+        _pip_exe = os.path.join(_venv_path, 'bin', 'pip')
+        _python_exe = os.path.join(_venv_path, 'bin', 'python')
+
+    else:
+        print(color("Setup error:", 'red'), "The virtual environment is unusable.")
+        print("To try to fix this, the "+bright(_venv_path)+" folder will be deleted and will be re-made next time.")
+        print("If it still doesn't work, please try "+bright(underline("updating to the latest Python version"))+".")
         input_exit()
 
-    venv.create(_venv_path, with_pip=True)
-    print(f"Setup: venv created at '{_venv_path}'")
-    print("\nIf this is your first time using ParaKit, welcome!")
-    print("Please give us a moment to finish setting up.\nIt should take about a minute.\n")
-    print("If it seems to do no progress at all for a while, try pressing Enter. Python's weird.")
+    #Install missing required packages in the venv
+    def get_installed_packages(venv_path):
+        result = subprocess.run([_pip_exe, "freeze"], capture_output=True, text=True)
+        return {line.split('==')[0] for line in result.stdout.splitlines()}
 
-if os.path.exists(os.path.join(_venv_path, 'Scripts')): #Windows
-    _pip_exe = os.path.join(_venv_path, 'Scripts', 'pip')
-    _python_exe = os.path.join(_venv_path, 'Scripts', 'python')
-elif os.path.exists(os.path.join(_venv_path, 'bin')): #Unix
-    _pip_exe = os.path.join(_venv_path, 'bin', 'pip')
-    _python_exe = os.path.join(_venv_path, 'bin', 'python')
-else:
-    print("Setup error: Python created an unusual or broken virtual environment.")
-    print("Try updating to the latest Python version.")
+    def get_required_packages(requirements_path):
+        with open(requirements_path) as reqs:
+            return {line.strip() for line in reqs if line and not line.startswith('#')}
+
+    if get_required_packages(_reqs_path) - get_installed_packages(_venv_path):
+        print(bright("Setup:"), "Missing packages detected; running pip install.")
+        try:
+            subprocess.check_call([_pip_exe, "install", "-r", _reqs_path])
+            print(bright("Setup:"), color("All required modules installed to '"+bright(_venv_path)+"'", 'green'))
+        except Exception as e:
+            print(color("Setup error:", 'red'), f"Error occurred while installing requirements.txt modules: {bright(e)}")
+            print("If you see this message, please contact the developer!")
+            input_exit()
+
+    #Run state-reader
+    subprocess.run([_python_exe, _worker_script_path])
+
+except KeyboardInterrupt:
+    print("Setup interrupted by user.")
     input_exit()
 
-#Install missing required packages in the venv
-def get_installed_packages(venv_path):
-    result = subprocess.run([_pip_exe, "freeze"], capture_output=True, text=True)
-    return {line.split('==')[0] for line in result.stdout.splitlines()}
+except Exception as e:
+    print(color("Uncaught setup error:", 'red'), e, darker())
+    traceback.print_exc()
+    print(default() + "If you see this message, please contact the developer!")
+    input_exit()
 
-def get_required_packages(requirements_path):
-    with open(requirements_path) as reqs:
-        return {line.strip() for line in reqs if line and not line.startswith('#')}
-
-if get_required_packages(_reqs_path) - get_installed_packages(_venv_path):
-    print("Setup: Missing packages detected; running pip install.")
-    try:
-        subprocess.check_call([_pip_exe, "install", "-r", _reqs_path])
-        print(f"Setup: All required modules installed to '{_venv_path}'")
-    except subprocess.CalledProcessError as e:
-        print(f"Setup error: Error occurred while installing requirements.txt modules: {e}")
-        input_exit()
-else:
-    print(f"Setup: Requirements all good.")
-
-#Run state-reader
-subprocess.run([_python_exe, _worker_script_path])
+finally:
+    if not valid_venv('Scripts') and not valid_venv('bin') and os.path.exists(_venv_path):
+        shutil.rmtree(_venv_path)
 
 #Inform users if running non-latest version
-subprocess.run([_python_exe, _vcheck_script_path])
+try:
+    subprocess.run([_python_exe, _vcheck_script_path])
+except KeyboardInterrupt:
+    pass
 
 #Prevent instant exit
 input_exit()
--- version_check.py
+++ version_check.py
@@ -1,61 +1,74 @@
-import requests 
+from utils import *
 from datetime import datetime, timezone, timedelta
 from settings import parakit_settings
 from version import VERSION_DATE
 
 try:
+    import requests
+except KeyboardInterrupt:
+    exit()
+
+try:
     _commits_response = requests.get('https://api.github.com/repos/Guy-L/parakit/commits?sha=master&per_page=100')
+except KeyboardInterrupt:
+    exit()
 except Exception as e:
-    print(f"\nVersion-checker: Failed to communicate with GitHub ({type(e).__name__}).")
+    print(bright("Version-checker:"), f"Failed to communicate with GitHub ({type(e).__name__}).")
     exit()
 
 try:
     _commits_response.raise_for_status()
 except requests.exceptions.HTTPError as e:
-    print(f"\nVersion-checker: HTTP error occurred: {e}")
+    print(bright("Version-checker:"), f"HTTP error occurred: {e}")
     exit()
-    
+
 _commits = _commits_response.json()
 _latest_version_date = datetime.strptime(_commits[0]['commit']['committer']['date'], '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
 
 if _latest_version_date > VERSION_DATE:
     if parakit_settings['minimize_version_checker']:
-    
+
         new_commits = 0
         for commit in _commits:
             commit_date = datetime.strptime(commit['commit']['committer']['date'], '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
             if commit_date > VERSION_DATE:
                 new_commits += 1
-    
-        print('\n================================')
-        print(f"A new version is available! ({new_commits} new commit{'s' if new_commits > 1 else ''})")
-        
-    else: 
-        print('\n================================')
-        print('A new version is available!')
-        print('New changes:')
-        
+
+        print(separator)
+        print(color(bright("A new version is available! ") + f"({new_commits} new change{'s' if new_commits > 1 else ''})", 'green'))
+
+    else:
+        new_commit_list = []
         new_commits = 0
         max_commits = 6
         max_commit_msg_len = 75
-        
+
         for commit in _commits:
             commit_date = datetime.strptime(commit['commit']['committer']['date'], '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
             if commit_date > VERSION_DATE:
                 new_commits += 1
-                if new_commits < max_commits:
+                if len(new_commit_list) < max_commits:
                     commit_msg = commit['commit']['message']
                     if len(commit_msg) > max_commit_msg_len:
                         commit_msg = commit_msg[:max_commit_msg_len] + '...'
-                        
-                    print(f"• [{commit_date.strftime('%Y-%m-%d %H:%M')} UTC] {commit_msg}")
-        
+
+                    new_commit_list.append(bp + bright(f"[{commit_date.strftime('%Y-%m-%d %H:%M')} UTC] ") + commit_msg)
+
+        print(separator)
+        s = ('s' if new_commits > 1 else '')
+        print(color(bright("A new version is available! ") + f"({new_commits} new change{s})", 'green'))
+        print(underline(bright(f'New change{s}:')))
+        print("\n".join(new_commit_list))
+
         if new_commits >= max_commits:
-            print(f"• ... and more! ({new_commits} commits)")
-        
-        print()
-        print('If you installed via Code > Download ZIP, please do so again')
-        print(f'or download a Release if one from after {_latest_version_date.strftime("%Y-%m-%d %H:%M")} UTC is available.')
-        print('If you installed via Git, please do \'git pull\'. You may need to resolve a merge')
-        print('conflict if changes were made to settings.py or analysis.py which conflict with yours.')
-        print('If this happens and you\'re not sure how to proceed, feel free to contact the developer.')
+            print(bp, italics(f"... and {'much ' if new_commits > 12 else ''}more!"))
+
+        if new_commits >= 3:
+            print()
+            print(bright("ParaKit is always improving. We recommend you update now."))
+            print(f"- If you installed with Git, simply enter \033[30;47mgit pull\033[0m.")
+            print(f"- If you installed by downloading the project files without")
+            print(f"  Git (i.e. Code -> Download ZIP), you should do so again.")
+            print(f"- Either way, because of the project's current setup,")
+            print(f"  you may need to resolve conflicts in {italics('analysis.py')} or")
+            print(f"  {italics('settings.py')}. {darker('There are plans to address this soon.')}")
